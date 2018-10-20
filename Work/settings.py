e8d86073568d02f0bc11745d97bc8d3363a91e48
crownedsilverfox <byskillcfpro@gmail.com>
2018-10-21 03:02:03 +0700

    static

--- Work/settings.py
+++ Work/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
