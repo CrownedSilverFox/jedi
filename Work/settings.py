7dbc545cca2f8a1fcc0a1849d73d2987110863d8
crownedsilverfox <byskillcfpro@gmail.com>
2018-10-21 11:53:37 +0700

    StaticFiles dir

--- Work/settings.py
+++ Work/settings.py
@@ -119,12 +119,14 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 
